fd5e69b2db7b3378c96c94d717bba90ebf14bca2
mostafa4460 <mostafa.nassr88@gmail.com>
2022-12-28 22:46:30 -0500

    add gradient and backgroundImage to all the screens

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StyleSheet } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        style={styles.rootScreen}
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  }
 });
